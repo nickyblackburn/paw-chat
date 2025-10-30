50b74addd575ba38b3c6009c85037e5952a765b8
nicky <nickkyblackburn@gail.com>
2025-10-30 11:37:15 -0400

    added base files working on ijmports

---
+++ README.md
@@ -0,0 +1,5 @@
+# Paw chat 
+* a mini chatbot im wwriting before my ai dev course starts
+
+## What can this chatbot do?
+1. 
---
+++ src/main.py
@@ -0,0 +1,11 @@
+# mainfile
+## base file for basic chatbot 
+
+import re, random, sys
+from datetime import date
+import difflib
+import spacy
+from rich.console import Console
+from rich.panel import Panel
+
+
